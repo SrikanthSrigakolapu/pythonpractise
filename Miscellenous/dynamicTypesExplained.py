2b6ed051f29ce68b202338b1a3f5b45e014e2251
ssrigakolapu <srikanth.srigakolapu@gmail.com>
2018-09-27 16:36:28 +0530

    New project Python Practise

---
+++ Miscellenous/dynamicTypesExplained.py
@@ -0,0 +1,15 @@
+# Python is a dynamic typed language because it checks the types at run time
+# whereas the static typed languages like Java checks the types at compile time itself
+#below is the example why Python is dynamic typed
+
+def dynamic(a):
+    if a > 0:
+        print("Passed \n\n")
+    else:
+        print("3"+a)
+
+#Below statement gets executed because the interpreter will not enter the error zone
+dynamic(1)
+
+#Below statement fails as the interpreter enters the error zone
+dynamic(-1)
