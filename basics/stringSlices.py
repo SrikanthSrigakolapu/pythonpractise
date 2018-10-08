a8955251deb6ed441d0fb4f167fee82791e88c71
ssrigakolapu <srikanth.srigakolapu@gmail.com>
2018-10-08 12:34:11 +0530

    new changes to file

--- basics/stringSlices.py
+++ basics/stringSlices.py
@@ -2,3 +2,6 @@
 sample ="Hello World"
 
 print("sample is the variable that contains string Hello World")
+print("Value of sample variable at 0", sample[0])
+print("Value of sample variable in between 0 and 2", sample[0:2])
+print("Value of sample variable in between 0 and 3", sample[0:3])
